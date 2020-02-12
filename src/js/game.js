84cc2aab4b3f9d190f3127e276568307ff4b4121
Alexey <vorresti@ya.ru>
2020-02-12 12:11:09 +0300

    init

---
+++ src/js/game.js
@@ -0,0 +1,21 @@
+import character from './domain';
+
+export default class Game {
+  start() {
+    console.log('game started');
+  }
+}
+
+class GameSavingData {
+
+}
+
+function readGameSaving() {
+
+}
+
+function writeGameSaving() {
+
+}
+
+export { GameSavingData, readGameSaving, writeGameSaving };
